4c9afb3f7b7d40c2dccae51bc2cb672490df8f76
ColonyRabbit <khanakorn01@gmail.com>
2025-05-15 11:53:48 +0700

    Initial Commit

---
+++ AppStarterKit/AppStarterKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStarterKitApp.swift
+//  AppStarterKit
+//
+//  Created by MacMini_M4-01 on 15/5/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct AppStarterKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStarterKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppStarterKit
+//
+//  Created by MacMini_M4-01 on 15/5/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
